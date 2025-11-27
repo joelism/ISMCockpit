54711bee747eec33a4229ed4907964e1b734be11
joelism <joel.weber1107@gmail.com>
2025-11-27 21:55:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+/* ISM Cockpit – police DB style, PIN-Login, Cases/Contacts/Reports
+   Build: policeDB5 – chronological reports, Files tab + Google Drive
+*/
+
+// Google Drive credentials (INSERTED)
+const GD = {
+  API_KEY: "AIzaSyCtd628byDsaRHu7mE_vj_gDedvTuUybFE",
+  CLIENT_ID: "990383497142-prtdauaqssaveqjls6em5c5ngtkrvtsn.apps.googleusercontent.com",
+  DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+  SCOPES: "https://www.googleapis.com/auth/drive.metadata.readonly"
+};
+
+/* ------------------------------
+  REST OF THE APP CODE REMOVED HERE FOR BREVITY
+  (You should paste your entire working app.js content here)
+--------------------------------*/
